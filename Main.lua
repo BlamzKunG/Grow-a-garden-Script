116a885db50f4cce529539d703b28ebc26f2ca81
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-20 17:44:37 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -6,18 +6,9 @@ local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
 
 --loadstring(game:HttpGet(""))()
 
-tab.newToggle("Kill All", "โอกาศโดนเตะน้อยมาก", false, function(all)
-    getgenv().FastAttack = all
+tab.newToggle("ขายทั้งหมด", "ขายของในตัวทั้งหมดทุก 10 วินาที", false, function(all)
+    getgenv().all = all
     if all then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Ka1t.lua"))()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Cr.lua"))()
-    end
-end)
-
-tab.newToggle("Kill All lnw", "โอกาศโดนเตะสูง", false, function(alll)
-    getgenv().FastAttack = alll
-    if alll then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/KA.lua"))()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/Cr.lua"))()
     end
 end)
