9ef0b18f0d40b729e5e1aecf1c48945a6d0f5a9a
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-20 18:40:23 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -6,7 +6,7 @@ local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
 
 --loadstring(game:HttpGet(""))()
 
-tab.newButton("ขายทั้งหมด", "ขายของในตัวทั้งหมดทุก 10 วินาที", function()
+tab.newButton("ขายทั้งหมด", "ขายของในตัวทั้งหมด", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/Gagg/refs/heads/main/Sellall.lua"))()
 end)
 
