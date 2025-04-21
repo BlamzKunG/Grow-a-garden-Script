b972c8456285673c3c5bbd8dfe76046004ceb4d4
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-04-21 15:03:29 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/Gagg/refs/heads/main/Ulcmgl.lua"))()
+
 local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
 local window = DrRayLibrary:Load("TikTok:Lxxuak", "Default")
 
