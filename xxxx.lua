417b3f09ac3539927bc462f916589386a593590f
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-06-05 08:08:54 +0700

    Update xxxx.lua

--- xxxx.lua
+++ xxxx.lua
@@ -6,13 +6,9 @@ local window = DrRayLibrary:Load("TikTok:Lxxuak", "Default")
 local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
 --loadstring(game:HttpGet(""))()
 
-tab.newButton("Kill All", "Kill all players", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/KA_N.lua"))()
-end)
-
-tab.newToggle("Auto ฟาร์ม", "เก็บผัก/ผลไม้ทั้งหมดในสวน + ขายของทั้งหมด", false, function(FullAutoFarm)
-    getgenv().FullAutoFarm = FullAutoFarm
-    if FullAutoFarm then
+tab.newToggle("Kill All", "Kill All Players", false, function(KillAllEnabled)
+    getgenv().KillAllEnabled = KillAllEnabled
+    if KillAllEnabled then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/KA_N.lua"))()
     end
 end)
