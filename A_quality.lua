9b72c92b6dd4dad98fc850995eb46354c939bdcf
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-06-15 07:16:48 +0700

    Update A_quality.lua

--- A_quality.lua
+++ A_quality.lua
@@ -4,9 +4,9 @@ local window = DrRayLibrary:Load("TikTok:Lxxuak", "Default")
 local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
 --loadstring(game:HttpGet(""))()
 
-tab.newToggle("Buy All", "Buy All", false, function(FullAutoFarm)
-    getgenv().FullAutoFarm = FullAutoFarm
-    if FullAutoFarm then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/Gagg/refs/heads/main/Kak.lua"))()
+tab.newToggle("Buy All", "Buy All", false, function(BuyAll)
+    getgenv().BuyAll = BuyAll
+    if BuyAll then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/Gagg/refs/heads/main/BuyAll"))()
     end
 end)
