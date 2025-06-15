9323df0c69a163f9b7cf8afd5ea6a3d669e66b80
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-06-15 07:15:11 +0700

    Update A_quality.lua

--- A_quality.lua
+++ A_quality.lua
@@ -4,9 +4,9 @@ local window = DrRayLibrary:Load("TikTok:Lxxuak", "Default")
 local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
 --loadstring(game:HttpGet(""))()
 
-tab.newToggle("Kill All", "Kill All Players", false, function(KillAllEnabled)
-    getgenv().KillAllEnabled = KillAllEnabled
-    if KillAllEnabled then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/My-roblox-sc/refs/heads/main/KA_N.lua"))()
+tab.newToggle("Buy All", "Buy All", false, function(FullAutoFarm)
+    getgenv().FullAutoFarm = FullAutoFarm
+    if FullAutoFarm then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BlamzKunG/Gagg/refs/heads/main/Kak.lua"))()
     end
 end)
